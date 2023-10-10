1091030f24a1cf46f48455a5ca4ef6a0ef6c3d81
AhmedMosleh <ahmedmosleh.8888@gmail.com>
2023-10-10 10:47:41 +0300

    project AskMe

--- Project_3_4_AskMe/project/main.cpp
+++ Project_3_4_AskMe/project/main.cpp
@@ -1,51 +1,119 @@
 #include <fstream>
 #include <iostream>
 #include <sstream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 string users_path = "users.txt";
+string names_path = "names.txt";
+string pw_path = "pw.txt";
+string unknown_path = "unknown.txt";
+
 string q_path = "q.txt"; // questions path
 
+auto status = ios::in | ios::out | ios::app;
+fstream fusers(users_path.c_str(), status);
+fstream fpw(pw_path.c_str(), status);
+fstream fnames(names_path.c_str(), status);
+fstream funknown(unknown_path.c_str(), status);
+
+//**************************************
+//********* user struct ***************
+//**************************************
 struct user {
-    string u_name;
-    int password;
-    string name;
-    string email;
-    bool anonymous;
-};
-
-int main() {
-  auto status = ios::in | ios::out | ios::app;
-
-  // cout << "Enter number in range 1-2: ";
-  string user_name, name, email;
+  string u_name{""};
   int password{};
+  string name{""};
+  string email{""};
   bool anonymous{};
 
+  void sign_up();
+};
+
+void user::sign_up() {
   cout << "Enter user name. (No spaces): ";
-  cin >> user_name;
-  cout << "Enter Password";
+  cin >> u_name;
+  fnames << u_name;
+  cout << "Enter Password: ";
   cin >> password;
-  cout << "Enter name";
+  fpw << password;
+  cout << "Enter name: ";
   cin >> name;
-  cout << "Enter email";
+  cout << "Enter email: ";
   cin >> email;
-  cout << "Allow anonymous";
+  cout << "Allow anonymous: ";
   cin >> anonymous;
+  funknown << anonymous;
 
-  fstream fusers(users_path.c_str(), status);
-  if(fusers.fail()) {
-    cout << "can't open\n";
-    return 0;
+}
+
+
+//**************************************
+//********* users struct ***************
+//**************************************
+struct users {
+
+  vector<string> urs_name;
+  vector<int> urs_pw;
+  vector<bool> urs_type;
+
+  void read_database();
+  void login();
+  void print_database();
+};
+
+void users::read_database() {
+  string temp_name {""};
+  string temp_pass{""};
+  string temp_unknown{""};
+
+  while (getline(fpw, temp_pass)) {
+    getline(fnames, temp_name);
+    getline(funknown, temp_unknown);
+    urs_pw.push_back(stoi(temp_pass));
+    urs_type.push_back(stoi(temp_unknown));
+    urs_name.push_back(temp_name);
   }
-  fusers << "\n";
-  fusers << user_name << "\n";
-  fusers << password << "\n";
-  fusers << name << "\n";
-  fusers << email << "\n";
-  fusers << anonymous << "\n";
+}
 
+void users::login() {
+  int idx {-1};
+  string in_name{""};
+  int in_password{};
+  cout << "Enter user name & password: ";
+  cin >> in_name >> in_password;
+  for (int i = 0; i < urs_name.size(); i++) {
+    if(urs_name.at(i) == in_name) {
+      idx = i;
+      break;
+    }
+  }
+  cout << urs_name.at(idx)  << " " << urs_pw.at(idx) << endl;
+  if (urs_pw.at(idx) == in_password) {
+    cout << "Welcome! " << urs_name.at(idx);
+  }
+}
+
+void users::print_database() {
+  for (int i = 0 ; i < urs_name.size(); i++) {
+    cout << urs_name.at(i) << " " << urs_pw.at(i) << endl;
+  }
+}
+
+
+//**************************************
+//********* main ***********************
+//**************************************
+
+int main() {
+  // users urs;
+  // urs.read_database();
+  // urs.login();
+  // urs.print_database();
+  user s;
+  s.sign_up();
 }
 
 // int sign_up()
