3641d62b11ccbe2d729e379c96b1717a79511413
AhmedMosleh <ahmedmosleh.8888@gmail.com>
2023-10-14 18:00:00 +0300

    ask.fm

--- Project_3_4_AskMe/project/main.cpp
+++ Project_3_4_AskMe/project/main.cpp
@@ -10,8 +10,8 @@ using namespace std;
 #define MAX_USER 10
 
 auto status = ios::in | ios::out | ios::app;
-string q_ids_path = "./q/q_ids.txt";
-fstream q_ids_file(q_ids_path.c_str(), status);
+string fmeta_path = "./q/q_meta_data.txt";
+fstream fmeta(fmeta_path.c_str(), status);
 
 bool loged_in{0};
 int my_id{55};
@@ -25,6 +25,7 @@ struct user {
   string name{""};
   string email{""};
   bool anonymous{0};
+  int u_id{};
 
   void read_data();
 };
@@ -32,12 +33,14 @@ struct user {
 void user::read_data() {
   cout << "Enter user name. (No spaces): ";
   cin >> u_name;
+  cout << "Enter user ID: ";
+  cin >> u_id;
   cout << "Enter Password: ";
   cin >> password;
-  cout << "Enter name: ";
-  cin >> name;
-  cout << "Enter email: ";
-  cin >> email;
+  // cout << "Enter name: ";
+  // cin >> name;
+  // cout << "Enter email: ";
+  // cin >> email;
   cout << "Allow anonymous: ";
   cin >> anonymous;
 }
@@ -48,6 +51,7 @@ void user::read_data() {
 struct ask_fm {
   // user users[MAX_USER];
   user temp;
+  void main_menu();
   void login();
   void sign_up();
   void print_q_to_me();
@@ -58,99 +62,175 @@ struct ask_fm {
   void list_users();
   void feed();
   void logout();
+
+  // helper functions:
+  bool is_user_exist();
+  bool is_q_exist();
+  bool
+  is_user_aq(); // this function checks if the user accepts anonymous questions?
 };
 
+void ask_fm::main_menu() {
+  int opt{};
+  do {
+    cout << "Menu:\n\t1: Login\n\t2: Sign Up\n";
+    cin >> opt;
+  } while (opt != 1 && opt != 2);
+
+  if (opt == 2) {
+    sign_up();
+  } else if (opt == 1) {
+    login();
+  }
+
+  cout << "Menu:\n\t"
+       << "1: Print Questions To Me\n\t"
+       << "2: Print Questions From Me\n\t"
+       << "3: Answer Question\n\t"
+       << "4: Delete Question\n\t"
+       << "5: Ask Question\n\t"
+       << "6: List System Users\n\t"
+       << "7: Feed\n\t"
+       << "8: Logout\n";
+  while (1) {
+    do {
+      cout << "Enter number in range 1 - 8:";
+      cin >> opt;
+    } while (!(1 <= opt && opt <= 8));
+
+    switch (opt) {
+    case 1:
+      print_q_to_me();
+      break;
+    case 2:
+      print_q_from_me();
+      break;
+    case 3:
+      answer_q();
+      break;
+    case 4:
+      delete_q();
+      break;
+    case 5:
+      ask_q();
+      break;
+    case 6:
+      list_users();
+      break;
+    case 7:
+      feed();
+      break;
+    case 8:
+      cout << "GOOD BEYE!" << endl;
+      return;
+      break;
+    }
+  }
+}
+
 void ask_fm::login() {
+  // 1. read user name and password:
   string in_name{""};
   int pw{};
   cout << "Enter user name & password: ";
   cin >> in_name >> pw;
+
+  // 2. check if there is a file with the user name
   ifstream fin("./u/" + in_name);
   if (fin.fail()) {
-    cout << "Can't open the file\n";
+    cout << "No user with this name. please try again\n";
     return;
   }
+  // @TODO: return to main menu if the file doesn't exist.
+
+  // 3. if the there is a file name then read the passward and compar2e:
   int pw_cmp{};
   fin >> pw_cmp;
   if (pw == pw_cmp) {
     cout << "Your're welcome!\n";
     loged_in = true;
   }
-  cout << loged_in << endl;
+  // @TODO: return to main menu if the file doesn't excist.
   fin.close();
 }
 
 void ask_fm::sign_up() {
+  // 1. read user name and password:
+
   temp.read_data();
   ofstream fout("./u/" + temp.u_name);
   fout << temp.password << endl;
   fout << temp.anonymous << endl;
+  fout << temp.u_id << endl;
   fout.close();
 }
+
 void ask_fm::print_q_to_me() {}
 void ask_fm::print_q_from_me() {}
+
 void ask_fm::answer_q() {}
 void ask_fm::delete_q() {}
 
-// The issue with your code is that after reading an integer for u_id, there is
-// a newline character left in the input buffer. When you subsequently call
-// getline to read the string q, it immediately reads the remaining newline
-// character and doesn't allow you to input the string.
+
 void ask_fm::ask_q() {
-  int u_id{};
+  int to_id{};
   cout << "Enter User id or -1 to cancel: ";
-  cin >> u_id;
-  if (u_id == -1) {
+  cin >> to_id;
+  if (to_id == -1) {
     return;
   }
-  cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-  string q;
-  cout << "Enter you Q: ";
-  getline(cin, q);
-
-  // generate a question id:
-  int q_id{};
-  string id_temp;
-
-  q_ids_file.seekg(0, ios::end);
-  streampos fileSize = q_ids_file.tellg();
-  if (fileSize == 0) {
-    q_id = 101;
-  } else {
-    while (getline(q_ids_file, id_temp)) {
-      cout << id_temp << endl;
-      q_id = stoi(id_temp);
-    }
-    q_id++;
+  // 1. check if the user exist:
+  bool is_exist = is_user_exist();
+  if (!is_exist) {
+    cout << "the user id isn't correct\n";
+    return;
   }
-  q_ids_file << q_id << endl;
-  cout << q_id << endl;
+  
+  // TODO:
+  // 2. if the user exist, check if he accept anonymous q?
+  bool is_aq = is_user_aq();
+  if (is_aq) {
+    cout << "Note: Anonymous questions are not allowed for this user\n";
+    return;
+  }
+
+  // 3. thread q:
+  int q_id{};
+  cout << "For thread Question: Enter Question id or -1 for new question: ";
+  cin >> q_id;
+
+  // TODO:
+  // 4. if it is thread q, check if the q exist:
+  if(q_id != -1) {
+    bool is_q = is_q_exist();
+  }
 
 
-  ofstream fout("./q/" + to_string(q_id) + "_" + to_string(my_id) + "_" +
-                to_string(u_id));
-  fout << "Q: " << q;
+  // 5. if the q isn't thread, read the q (and q id for first version)
+  string q {""};
+  cin.ignore();
+  getline(cin, q);
+  cin >> q_id;
+
+  // 6. 
+  
+
+
+  //
 }
 void ask_fm::list_users() {}
 void ask_fm::feed() {}
-void ask_fm::logout() {}
+
+bool ask_fm::is_user_exist() { return 1; }
+bool ask_fm::is_q_exist() { return 1; }
+bool ask_fm::is_user_aq() { return 1; }
 //**************************************
 //********* main ***********************
 //**************************************
 
 int main() {
   ask_fm sys;
-  sys.ask_q();
-  sys.ask_q();
-  sys.ask_q();
-
-
-  // sys.sign_up();
-  // sys.login();
-  // while(1);
-  // string s;
-  // getline(cin, s);
-  // cout << s;
+  sys.main_menu();
 }
 
 // int sign_up()
